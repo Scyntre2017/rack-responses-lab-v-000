e3bab3110abb6b72a6ea97e71da110a1f04551e1
Christopher Jones <cjones999983@gmail.com>
2018-07-14 23:25:08 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,16 +5,6 @@ class Application
 
     binding.pry
 
-    resp.write "#{num_1}\n"
-    resp.write "#{num_2}\n"
-    resp.write "#{num_3}\n"
-
-    if num_1==num_2 && num_2==num_3
-      resp.write "You Win"
-    else
-      resp.write "You Lose"
-    end
-
     resp.finish
   end
 
