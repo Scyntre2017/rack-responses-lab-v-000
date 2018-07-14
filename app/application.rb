0833a0d4cfcb575d8d6d975ef9ba618a0cec84bf
Christopher Jones <cjones999983@gmail.com>
2018-07-14 23:33:16 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,6 +4,12 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    if Time.now.to_s.split[1].to_i <= 12
+      resp.write "Good Morning"
+    elsif Time.now.to_s.split[1].to_i > 12
+      resp.write "Good Afternoon"
+    end
+
     binding.pry
 
     resp.finish
